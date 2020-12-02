cba923e59e5daefdadd3c5a21ea053ce409ad982
couy <couy69@gmail.com>
2020-12-02 09:55:13 +0800

    添加gzip,压缩文件体积试图加快访问速度

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
+const CompressionPlugin = require("compression-webpack-plugin")
 module.exports = {
   // 选项...
-  publicPath:'',
-  productionSourceMap:false,
+  publicPath: '',
+  productionSourceMap: false,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|.\css/, //匹配文件名
+            threshold: 1024, //对超过10k的数据压缩
+            deleteOriginalAssets: false //是否删除源文件
+          })
+        ]
+      }
+    }
+  },
 }
